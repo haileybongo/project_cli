ebf476951710a4a865d4b39ea456356e0d3ef0a5
Hailey Bongo <haileyebongo@gmail.com>
2020-03-12 23:33:53 +0000

    Adding notes

--- lib/plant_cli/cli.rb
+++ lib/plant_cli/cli.rb
@@ -4,4 +4,10 @@ class PlantCli::CLI
     puts "test"
   end
   
+  
+  #my key =SlY3S2tVa0Fja0x1b1l2bFY2Zm9tdz09
+  # https://trefle.io/api/plants?token=SlY3S2tVa0Fja0x1b1l2bFY2Zm9tdz09
+  #gets search term = https://trefle.io/api/plants?q=rosemary&token=SlY3S2tVa0Fja0x1b1l2bFY2Zm9tdz09
+  #after ID is picked, pulls info for ID = https://trefle.io/api/plants/141476?token=SlY3S2tVa0Fja0x1b1l2bFY2Zm9tdz09
+  
 end
