1584c88f70d5f63645f156f7dc84dbbaf303fa5a
Hailey Bongo <haileyebongo@gmail.com>
2020-03-13 17:44:31 +0000

    Updates

--- lib/recipe_cli/api.rb
+++ lib/recipe_cli/api.rb
@@ -6,10 +6,11 @@ class RecipeCli::API
   def fetch(string)
     url = "https://api.edamam.com/search?q=#{string}&app_id=f6f7d13e&app_key=3e087fb68f68af8ea863608aa9f7d797"
     response = HTTParty.get(url)
-    binding.pry 
+    #binding.pry 
     puts"Select Recipe From Available List"
     response["hits"].each do |recipe|
-      puts recipe["name"]
+      name = recipe["recipe"]["yield"] 
+      puts name 
     end
   end
 end
