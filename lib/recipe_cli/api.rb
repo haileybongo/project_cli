d1ddc7c8adeeb59b9e3ddb8e3d47f270f10c7c93
Hailey Bongo <haileyebongo@gmail.com>
2020-03-13 06:29:42 +0000

    Updates

--- lib/recipe_cli/api.rb
+++ lib/recipe_cli/api.rb
@@ -1,8 +1,11 @@
+require 'httparty'
+require 'pry'
+
 class RecipeCli::API 
   
   def fetch(string)
     url = "https://api.edamam.com/search?q=#{string}&app_id=f6f7d13e&app_key=3e087fb68f68af8ea863608aa9f7d797"
-    respone = HTTPParty.get(url)
+    response = HTTParty.get(url)
     binding.pry 
   end
 end
@@ -11,3 +14,7 @@ end
 
 
   #gets search term = https://api.edamam.com/search?q= pasta,tomato,sausage &app_id=f6f7d13e&app_key=3e087fb68f68af8ea863608aa9f7d797
+  
+  #./bin/recipe-project
+  #response["hits"][0]
+  
