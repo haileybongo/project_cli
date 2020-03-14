752e87109594839a5f364be6bcf27f24319ccbd8
Hailey Bongo <haileyebongo@gmail.com>
2020-03-14 05:33:55 +0000

    Updates

--- lib/recipe_cli/cli.rb
+++ lib/recipe_cli/cli.rb
@@ -1,7 +1,26 @@
 class RecipeCli::CLI
   
   def call 
-    puts "test"
+    puts "Hello! Welcome to Recipe Search. What ingredients would you like to search for?"
+     food = ""
+     food = gets.chomp!
+    
+    puts "Do you have dietary restrictions you would like to consider? Enter Y or N"
+    
+    dietary = ""
+    dietary = gets.chomp!
+     if dietary = "Y"
+      puts "Choose from restrictions below:"
+      puts "1. Vegetarian"
+      puts "2. Vegan"
+      puts "3. Tree Nut Free"
+      puts "4. Peanut Free"
+      puts "5. No Soy"
+      puts "6. Dairy Free"
+      puts "7. Gluten Free"
+      
+      restrictions = gets.chomp!
+   
   end
   
 
