ca8fdac13391e9bcd1dbc9059a38a331fe4733d1
Hailey Bongo <haileyebongo@gmail.com>
2020-03-15 19:52:26 +0000

    removing intialize from Recipes class

--- lib/recipe_cli/recipes.rb
+++ lib/recipe_cli/recipes.rb
@@ -1,21 +1,7 @@
 class RecipeCli::Recipes
   
     attr_accessor :chosen_recipe, :name, :link, :recipe_yield, :ingredientLines, :health_labels, :calories, :response, :url, :key_words, :user_health, :user_cals
-    
-   @@all = []
-  
-  def initialize(string, health = nil, calories = nil)
-    @key_words = string
-    @user_health = health 
-    @user_cals = calories
-    self.fetch 
-    response = HTTParty.get(@url)
-    @@all << self 
-  end
-  
-  def all 
-    @@all
-  end
+
   
   def self.select_recipe
     
